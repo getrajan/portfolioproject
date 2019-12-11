996c5edbf58b62903647565f94f8a6675630a64e
Rajan <rajankarki0401@gmail.com>
2019-12-11 21:18:57 +0545

    jobapp up and runnign

--- jobapp/admin.py
+++ jobapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Job # import class Job from models.py
 
 # Register your models here.
+
+admin.site.register(Job)
+
+
--- jobapp/models.py
+++ jobapp/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/', height_field=None, width_field=None)
     summary = models.CharField( max_length=250)
+    
+    def __str__(self):
+        return self.summary[:20]+"......."
+    
